f1782a510394cd376c408ed4f5ae33276c5f5a98
Hala Abusalim <82084034+HalaSalim77@users.noreply.github.com>
2025-08-25 16:37:35 +0300

    Update site URL in astro.config.mjs to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://astro.theqalink.com',
   i18n: {
     defaultLocale: 'ar',
     locales: ['ar'],
